4e1dc9d79f37a160bc4c4b61630b544419fc5756
mandragora-hub <royer01gomez@gmail.comn>
2024-06-23 10:14:19 -0400

    feat: using cache in memory

--- app/main.py
+++ app/main.py
@@ -1,12 +1,24 @@
+from contextlib import asynccontextmanager
 from typing import Annotated
 from fastapi import FastAPI, Query
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import FileResponse
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.inmemory import InMemoryBackend
+from fastapi_cache.decorator import cache
 
-from app import utils;
+from app import utils
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Initialize FastApi cache.
+    FastAPICache.init(InMemoryBackend())
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 origins = ["*"]  # allow all origins
 app.add_middleware(
@@ -19,12 +31,14 @@ app.add_middleware(
 
 
 @app.get("/")
+@cache()
 async def read_index():
     return FileResponse('static/index.html')
 
 
 @app.get("/metadata/")
-def read_item(url: Annotated[str, Query(description="URL where a pdf file is located.", min_length=5, max_length=100)]):
+@cache(expire=604800)  # one week
+def get_metadata(url: Annotated[str, Query(description="URL where a pdf file is located.", min_length=5, max_length=100)]):
     content = utils.read_file_from_url(url)
     result = utils.extract_pdf_metadata(content)
     return result
